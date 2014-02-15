cf3ff2517250ac7857a824d405cc57dbbeccafc8
sinius15 <sinius15@gmail.com>
2014-02-15 19:27:02 +0100

    new idea for the leveling    starting to change stuff up

--- src/com/sinius15/pamapeditor/gui/FramePrisonEditor.java
+++ src/com/sinius15/pamapeditor/gui/FramePrisonEditor.java
@@ -1,26 +1,23 @@
 package com.sinius15.pamapeditor.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Rectangle;
 
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.DefaultListModel;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.TitledBorder;
 
 import com.sinius15.pamapapi.DataBlock;
 import com.sinius15.pamapapi.Entry;
@@ -32,22 +29,30 @@ public class FramePrisonEditor extends JFrame {
 	private static final long serialVersionUID = 7539789100657354803L;
 	private JPanel contentPane;
 	private JTextField prisonNameField;
-	private JPanel dataContainer;
 	private JScrollPane entryScroller;
 	private JPanel buttonContainer;
 	private JPanel propetyContainer;
 	private JButton btnSavePrison;
 	private JPanel entryHolder;
 	private JButton btnIgnoreChanges;
-	private JList<DataBlock> levelList;
-	private JPanel entryPane;
 	private JButton btnSavePrisonAs;
 	private JPanel bottomContainer;
 	private JLabel lbl1;
-	private JPanel levelPane;
-	private DefaultListModel<DataBlock> listModel = new DefaultListModel<>();
+	private JSplitPane splitPane;
+	private JScrollPane levelScroller;
+	private JPanel levelHolder;
+	private JToggleButton tglbtnNewToggleButton;
+	private JToggleButton tglbtnNewToggleButton_1;
+	private JToggleButton tglbtnNewToggleButton_2;
+	private JToggleButton tglbtnNewToggleButton_3;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	public FramePrisonEditor(Prison prison) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
 		setTitle("A Prison Editor!");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 670, 578);
@@ -56,47 +61,45 @@ public class FramePrisonEditor extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		dataContainer = new JPanel();
-		contentPane.add(dataContainer);
-		dataContainer.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		levelPane = new JPanel();
-		dataContainer.add(levelPane);
-		levelPane.setAlignmentY(Component.TOP_ALIGNMENT);
-		levelPane.setPreferredSize(new Dimension(300, 386));
-		levelPane.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Level", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		levelPane.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		levelPane.setLayout(new BorderLayout(0, 0));
-		
-		levelList = new JList<DataBlock>(listModel);
-		levelList.setCellRenderer(new DefaultListCellRenderer() {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-				super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-				if(value instanceof DataBlock)
-					setText(((DataBlock) value).name);
-				return this;
-			}
-			
-		});
-		levelPane.add(levelList, BorderLayout.CENTER);
-		
-		entryPane = new JPanel();
-		dataContainer.add(entryPane);
-		entryPane.setAlignmentY(Component.TOP_ALIGNMENT);
-		entryPane.setAlignmentX(1.0f);
-		entryPane.setBorder(new TitledBorder(null, "Entrys", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		entryPane.setPreferredSize(new Dimension(315, 386));
-		entryPane.setLayout(new BorderLayout(0, 0));
+		splitPane = new JSplitPane();
+		contentPane.add(splitPane, BorderLayout.CENTER);
 		
 		entryScroller = new JScrollPane();
-		entryPane.add(entryScroller, BorderLayout.CENTER);
+		splitPane.setRightComponent(entryScroller);
 		entryScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		entryHolder = new JPanel();
 		entryScroller.setViewportView(entryHolder);
-		entryHolder.setLayout(new GridLayout(0,1));
+		entryHolder.setLayout(new GridLayout(0,1,0,0));
+		
+		levelScroller = new JScrollPane();
+		levelScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		levelScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		splitPane.setLeftComponent(levelScroller);
+		
+		levelHolder = new JPanel();
+		levelScroller.setViewportView(levelHolder);
+		levelHolder.setLayout(null);
+		
+		tglbtnNewToggleButton = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton.setBounds(0, 1, 121, 110);
+		buttonGroup.add(tglbtnNewToggleButton);
+		levelHolder.add(tglbtnNewToggleButton);
+		
+		tglbtnNewToggleButton_1 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_1.setBounds(0, 111, 121, 110);
+		buttonGroup.add(tglbtnNewToggleButton_1);
+		levelHolder.add(tglbtnNewToggleButton_1);
+		
+		tglbtnNewToggleButton_2 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_2.setBounds(0, 221, 121, 110);
+		buttonGroup.add(tglbtnNewToggleButton_2);
+		levelHolder.add(tglbtnNewToggleButton_2);
+		
+		tglbtnNewToggleButton_3 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_3.setBounds(0, 331, 121, 110);
+		buttonGroup.add(tglbtnNewToggleButton_3);
+		levelHolder.add(tglbtnNewToggleButton_3);
 		
 		bottomContainer = new JPanel();
 		contentPane.add(bottomContainer, BorderLayout.SOUTH);
@@ -133,14 +136,12 @@ public class FramePrisonEditor extends JFrame {
             entryHolder.scrollRectToVisible(rect);
 		}
 		for(DataBlock b : prison.dataBlocks){
-			//listModel.addElement(b);
+			
 		}
-		listModel.addElement(new DataBlock("eyo"));
 		revalidate();
 		setVisible(true);
-	}
-	public JPanel getDataContainer() {
-		return dataContainer;
+		
+		splitPane.setDividerLocation(0.5);
 	}
 	public JScrollPane getEntryScroller() {
 		return entryScroller;
@@ -163,9 +164,6 @@ public class FramePrisonEditor extends JFrame {
 	public JTextField getPrisonNameField() {
 		return prisonNameField;
 	}
-	public JPanel getEntryPane() {
-		return entryPane;
-	}
 	public JButton getBtnSavePrisonAs() {
 		return btnSavePrisonAs;
 	}
@@ -175,7 +173,7 @@ public class FramePrisonEditor extends JFrame {
 	public JLabel getLbl1() {
 		return lbl1;
 	}
-	public JPanel getLevelPane() {
-		return levelPane;
+	public JPanel getLevelHolder() {
+		return levelHolder;
 	}
 }
